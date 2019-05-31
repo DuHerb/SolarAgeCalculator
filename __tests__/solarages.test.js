e4d5f4039939c97840aa7d376859ded9847078d1
Dustin Herboldshimer <dustnpdx@gmail.com>
2019-05-31 12:48:57 -0700

    add functions and tests for class Solarage, inYears(), inDays(), and getPlanet()

--- __tests__/solarages.test.js
+++ __tests__/solarages.test.js
@@ -1,12 +1,38 @@
 /* eslint-disable no-unused-vars */
-import { greet } from './../src/solarages';
+import { Solarage } from './../src/solarages';
 import { isTSAnyKeyword, exportAllDeclaration } from '@babel/types';
 
-describe('solarage', function() {
 
-  test('should return a string object', () => {
-    expect(typeof greet()).toEqual('string');
+describe('solarage', function() {
+  test('should return true if input is date object', () => {
+    let input = new Date();
+    expect(typeof input).toBe('object');
   });
+
+  test('should return valid class property types/values from constructor', () => {
+    let solarage = new Solarage("12-18-1980");
+    expect(typeof solarage.bday).toBe('object');
+    expect(typeof solarage.now).toBe('object');
+    expect(solarage.now.getFullYear()).toEqual(2019);
+    expect(solarage.bday.getFullYear()).toEqual(1980);
+  });
+
+  test('should return users age in years', () => {
+    let earthAge = new Solarage("12-18-1980").inYears();
+    expect(Math.floor(earthAge)).toEqual(38);
+  });
+
+  test('should return users age in days', () => {
+    let earthAge = new Solarage("12-18-1980").inDays();
+    expect(Math.floor(earthAge)).toEqual(14043);
+  });
+
+  test('should return the correct planet by string', () => {
+    let userString = "Jupiter";
+    let solarage = new Solarage("12-18-1980");
+    expect(solarage.getPlanet(userString)).toEqual(solarage.planets["Jupiter"]);
+  });
+
 
 });
 
--- src/solarages.js
+++ src/solarages.js
@@ -1,3 +1,28 @@
-export function greet(){
-  return "Solar Age Calculater(from solarages.js)";
+export class Solarage {
+  constructor(bdayString) {
+    this.bday = new Date(bdayString);
+    this.now = new Date();
+    this.age = Math.abs(this.now - this.bday);
+    this.planets = {
+      "Mercury": 88,
+      "Venus": 225,
+      "Mars": 687,
+      "Jupiter":(11.8*365),
+      "Saturn":(29.4*365),
+      "Uranus":(84*365),
+      "Neptune":(164*365)
+    };
+  }
+
+  inYears(){
+    return this.age/(365*24*60*60*1000);
+  }
+
+  inDays(){
+    return this.age/(24*60*60*1000);
+  }
+
+  getPlanet(planetOptionValue){
+    return this.planets[planetOptionValue];
+  }
 }
