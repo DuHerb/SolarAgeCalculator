0259ad65ae9a7a38ae92097282a73be60c03ab0f
Dustin Herboldshimer <dustnpdx@gmail.com>
2019-06-01 20:10:12 -0700

    add spaces for readability

--- src/index.js
+++ src/index.js
@@ -8,9 +8,11 @@ $(document).ready(function(){
   $('#userSubmit').on('click', function(e){
     e.preventDefault();
     $('output').empty();
+
     let userPlanetOption = $('#planetInput').val();
     let solarage = new Solarage($('#userBday').val());
     let lifeExpectancey = $('#lifeExpectancy').val();
+
     if(!isNotFutureDate($('#userBday').val())){
       $('#planetAge').text("Hello!? McFly!? You haven't been born yet. Re-enter your real birthday");
     }
